fcc936b8215f3154d7deae17cc937d7acbec492a
Engineer Green <yurii.borovchuk@growe.com>
2025-04-30 12:37:55 +0300

    Prepare for symfony receipt

--- src/SeoBundle.php
+++ src/SeoBundle.php
@@ -2,8 +2,14 @@
 
 namespace Gurtok\SeoBundle;
 
+use Gurtok\SeoBundle\DependencyInjection\GurtokSeoExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class SeoBundle extends Bundle
 {
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        return new GurtokSeoExtension();
+    }
 }
